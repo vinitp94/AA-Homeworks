2d045f1938e577c25124432df6373930a25f8b59
vinit_parikh <vinitp94@gmail.com>
2016-12-02 20:36:05 -0800

    Complete library controllers

--- w4d1/Library Controller/app/controllers/books_controller.rb
+++ w4d1/Library Controller/app/controllers/books_controller.rb
@@ -1,21 +1,30 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
+  
   def book_params
     params.require(:book).permit(:title, :author)
   end
